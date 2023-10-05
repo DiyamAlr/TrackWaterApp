243a53975d7f6143514f93a21ff6f0f445e03c2f
DiyamAlr <Deemrabah99@gmail.com>
2023-10-05 12:11:17 +0300

    Initial Commit

---
+++ TrackWaterApp/TrackWaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackWaterAppApp.swift
+//  TrackWaterApp
+//
+//  Created by Diyam Alrabah on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TrackWaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
