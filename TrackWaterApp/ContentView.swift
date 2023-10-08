c2a7b953bf4ba9c9afc585aed927d0ba6aa64ec3
DiyamAlr <Deemrabah99@gmail.com>
2023-10-08 11:54:40 +0300

    WaterTrack

--- TrackWaterApp/ContentView.swift
+++ TrackWaterApp/ContentView.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Water Tracker 💦")
+                .fontWeight(.heavy)
+            
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+            Text("Apple Health")
+            }
+
+            Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Range@*/1...10/*@END_MENU_TOKEN@*/) {
+            Text("Cups to drink  ber day 0")
+            }
+            .padding(.top)
+            
+            Button("Cuntinue") {
+                
+            }
+            .padding(.top)
+            
         }
         .padding()
     }
